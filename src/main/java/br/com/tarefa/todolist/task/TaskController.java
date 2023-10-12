b527c435dcd0b46b744d89881db61aabde40528c
tati <tati@uol.com.br>
2023-10-11 23:26:12 -0300

    feat: Aula
    [aula 1, 2 e 3]

---
+++ src/main/java/br/com/tarefa/todolist/task/TaskController.java
@@ -0,0 +1,23 @@
+package br.com.tarefa.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+
+        TaskModel task = taskRepository.save(taskModel);
+        return ResponseEntity.ok(task);
+    }
+}
---
+++ src/main/java/br/com/tarefa/todolist/user/UserController.java
@@ -0,0 +1,53 @@
+package br.com.tarefa.todolist.user;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    /**
+     * String (texto)
+     * Integer (int) numeros inteiros
+     * Double (double) Números 0.0000
+     * Float (float) Números 0.0000
+     * char (A C)
+     * Date (data)
+     * void
+     */
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        UserModel user = userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+
+}

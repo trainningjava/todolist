3881a91a7eb9fe2851c14aaf0954a7258e105d65
tati <tati@uol.com.br>
2023-10-13 15:15:43 -0300

    feat: Aula
    [-atualizando o readme
    -aula 5
    -tratamento de erro]

--- src/main/java/br/com/tarefa/todolist/task/TaskModel.java
+++ src/main/java/br/com/tarefa/todolist/task/TaskModel.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
